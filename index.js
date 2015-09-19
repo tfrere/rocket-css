ae7300fc72211fc57fabbf8935a83f2cce13da73
poupi <tfrere>
2015-09-19 18:10:07 +0200

    fix server

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use('/', express.static('public'));
-app.listen(3000, function() { console.log('listening')});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
