741e5b980cc89eb241658757ed1a62f0778ac127
poupi <tfrere>
2015-11-02 23:45:03 +0100

    release landing 1.0

--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -2,7 +2,7 @@
   return function () {
     gulp.watch([paths.styles.watch], ['style']);
     gulp.watch([paths.templates.watch], ['jade']);
-    gulp.watch([paths.assets.watch, paths.fonts.watch], ['assets']);
+    //gulp.watch([paths.assets.watch, paths.fonts.watch], ['assets']);
     gulp.watch([paths.vendor.watch], ['vendor']);
     gulp.watch(paths.scripts.watch, ['scripts']);
     gulp.watch(paths.ressources.watch, ['ressources']);
