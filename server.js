3eb976af3c8be7cef42acee39c159659dad0c667
poupi <tfrere>
2016-01-24 19:44:15 +0100

    update serv port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-
+app.set('port', (process.env.PORT || 5000));
 app.use('/', express.static(__dirname + '/build/'));
 
 app.all("/*", function(req, res, next) {
