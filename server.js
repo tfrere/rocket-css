21ec98f824e8c812782eecead9b3d2c33540cffa
scandalousman <dandy.antibes@gmail.com>
2017-01-24 16:47:29 +0100

    add root static file nodeserver and create new nodeserver for api Edetmoi. Add app on index html for jquery. Move bower.json on dist

--- server.js
+++ server.js
@@ -10,8 +10,9 @@ app.set('port', (process.env.PORT || 5000));
 app.use(cors());
 
 app.use('/', express.static(__dirname + '/dist/'));
-app.use('/', express.static(__dirname + '/sitemap/'));
-app.use("/vendor", express.static(__dirname + "/dist/vendor"));
+//app.use('/', express.static(__dirname + '/app/'));
+/*app.use('/', express.static(__dirname + '/sitemap/'));
+/*app.use("/vendor", express.static(__dirname + "/dist/vendor"));
 app.use("/bower_components", express.static(__dirname + "/bower_components"));
 app.use("/scripts", express.static(__dirname + "/dist/scripts"));
 app.use("/ressources", express.static(__dirname + "/dist/ressources"));
@@ -23,12 +24,28 @@ app.get('/test', function(req,res){
 		return res.send(200);
 });
 
-app.post('/sendQuestion', function(req,res){
+app.put('/sendForm', function(req,res){
 	console.log(req.body);
-	if (req.body.email && req.body.name && req.body.content)
-		return res.send(200);
-	else
-		return res.send(404, error.code);
+	var herv = req.body.test;
+	res.status(200).send(herv);
+});
+
+//app.use('\/[^\.]+$', express.static(__dirname + '/dist/'));
+/*app.get('\/[^\.]+$', function(req, res){
+   //res.set('Content-Type', 'text/html')
+   //   .sendfile("index.html", { root: __dirname + "/dist" });
+   res.send('test');
+});*/
+
+var api = express();
+api.use(bodyParser.json());
+api.put('/api/sendForm', function(req,res){
+	console.log(req.body);
+	res.status(200).send(req.body);
+});
+
+api.listen(4000, function () {
+	console.log(4000);
 });
 
 // SAY HELLO
